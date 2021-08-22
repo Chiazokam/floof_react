bec3c8fd30eeb77381292bbaaabdc08a56e946de
chiazokam <chiazokamecheta@gmail.com>
2021-08-22 11:01:20 +0100

    chore: Fetch Data Using React Query
    - fetch data using react query and axios

--- src/components/Bank/BankList.tsx
+++ src/components/Bank/BankList.tsx
@@ -1,23 +1,39 @@
 import React, { useState } from 'react';
-import banks from './fake';
+import axios from 'axios';
+import { useQuery } from 'react-query';
 import Bank from './Bank';
+import Loader from '../Loader/Loader';
 import styles from './Bank.module.css';
 
-const BankList = () => {
+const fetchBanks = async (query: string | null) => {
+  const { data } = await axios.get(`https://floof-backend.herokuapp.com/api/v1/banks${query ? `?search=${query}` : ''}`);
+  return data;
+};
+
+interface BankListProps {
+  searchValue: string | null;
+}
+
+const BankList = ({ searchValue }: BankListProps) => {
   const [activeTab, setActiveTab] = useState('');
+  const { data, isLoading, error } = useQuery(['banks', searchValue], () => fetchBanks(searchValue));
 
   const toggleAccordion = (id: string) => {
     setActiveTab(id);
   };
+  const banks = (data && data?.data?.rows) ?? [];
 
   return (
     <div className={styles.bank_list_container}>
       <div className={styles.bank_list}>
-        <div>
-          {banks.map((bank: BankType) => (
-            <Bank bank={bank} activeTab={activeTab} toggleAccordion={toggleAccordion} />
-          ))}
-        </div>
+        {(error || banks.lenght === 0) && <span>No Banks Found</span>}
+        {isLoading ? <Loader /> : (
+          <>
+            {banks.map((bank: BankType) => (
+              <Bank bank={bank} activeTab={activeTab} toggleAccordion={toggleAccordion} />
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
